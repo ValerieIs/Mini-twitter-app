92a85de4ddacf3f0c19bd4ca726c12b56786c203
Valeria Isaeva <valevs.isv@gmail.com>
2020-10-21 20:26:51 +0500

    Updates

---
+++ precache-manifest.81735af900cc9858795772ad2f41115f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3d079deb5eaf9b4033e428ec8fa6e7",
+    "url": "/Mini-twitter-app/index.html"
+  },
+  {
+    "revision": "ae3bd0ae475960928599",
+    "url": "/Mini-twitter-app/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d72385432bd690b282fd",
+    "url": "/Mini-twitter-app/static/css/main.c66c6057.chunk.css"
+  },
+  {
+    "revision": "ae3bd0ae475960928599",
+    "url": "/Mini-twitter-app/static/js/2.26895866.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Mini-twitter-app/static/js/2.26895866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72385432bd690b282fd",
+    "url": "/Mini-twitter-app/static/js/main.bf096303.chunk.js"
+  },
+  {
+    "revision": "5605f978d8e24578de1e",
+    "url": "/Mini-twitter-app/static/js/runtime-main.6aa136c9.js"
+  }
+]);
